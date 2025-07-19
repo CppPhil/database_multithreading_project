963171a4f94391babc6a875af3a0d573c30325ac
Phillip Schackier <CppPhil@users.noreply.github.com>
2025-07-19 21:06:01 +0200

    Initial Commit

---
+++ format.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Directory containing this bash script.
+readonly DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+echo "Formatting C++ code..."
+find "$DIR/src" -type f \( -name "*.cpp" -o -name "*.hpp" \) -exec clang-format -i {} +
+find "$DIR/include" -type f \( -name "*.cpp" -o -name "*.hpp" \) -exec clang-format -i {} +
+echo "Formatted C++ code."
+
+echo "Formatting CMake code..."
+find "$DIR" -type f \( -name "CMakeLists.txt" \) -not \( -path "*external*" -o -path "*build*" \) -exec cmake-format -i {} +
+echo "Formatted CMake code."
+
+echo "Done. Exiting format.sh."
+
+exit 0
+
